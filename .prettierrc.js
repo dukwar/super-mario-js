5059237a166e0e2219e870c21454a255e36a298d
Dmitrii_Klopov <dmitrii.klopov@stm-labs.ru>
2024-06-17 16:11:59 +0300

    добавлен карframe added

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: false,
+  arrowParens: 'always',
+};
